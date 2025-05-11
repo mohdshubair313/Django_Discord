a6df82ea80b6ed5b122ba6949d3020e78bc7e1be
mohdshubair313 <shubair313@gmail.com>
2025-05-11 22:56:56 +0530

    added soem models

--- base/models.py
+++ base/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Room(models.Model):
+    # host = models.ForeignKey('User', on_delete=models.SET_NULL, null=True)
+    # topic = models.ForeignKey('Topic', on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    updated = models.DateTimeField(auto_now=True) # take a snapshot and changes every single timeq
+    created = models.DateTimeField(auto_now_add=True) # take a snapshot and changes only when the object is created
+
+    def __str__(self):
+        return self.name
--- base/views.py
+++ base/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from models import Room
 # Create your views here.
+
+def home(request):
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
+    return render(request, '', context)
+    # return render(request, 'base/home.html', context)
+
+def room(request):
+    return render(request, '')
